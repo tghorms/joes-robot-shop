971a8e1857d7d39e3f0ee7a5028ef5b138cac23c
Thomas Ghormley <tghorms@gmail.com>
2024-04-05 14:21:32 -0700

    creating angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './user/template-form-controls/template-form-controls.component';
+
 
 @NgModule({
   declarations: [
@@ -20,8 +23,9 @@ import { SignInComponent } from './user/sign-in/sign-in.component';
     ProductDetailsComponent,
     CartComponent,
     SignInComponent,
+    TemplateFormControlsComponent
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IUserCredentials } from '../user.model';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-sign-in',
@@ -6,7 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent {
+  credentials : IUserCredentials = {
+    email: '',
+    password: ''
+  };
+  signInError: boolean = false;
 
-  constructor() { }
+  constructor(
+    private userSvc: UserService,
+    private router: Router
+  ) { }
 
+  signIn() {
+    this.signInError = false;
+    this.userSvc.signIn(this.credentials).subscribe({
+      next: () => this.router.navigate(['/catalog']),
+      error: () => (this.signInError = true)
+    });
+  }
 }
